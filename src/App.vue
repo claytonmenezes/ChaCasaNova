e1b6097ba7b3b98582bf817ffc676ca719159c7f
Clayton Menezes dos Santos <claytonmenezes@hotmail.com>
2024-10-30 11:04:48 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="bg-[#a38967]">
+    <div class="grid grid-cols-12">
+      <div class="col-span-3 items-center grid justify-center">
+        <div class="col-span-12">
+          <img src="/pote.png" alt="Pote" class="rotate-12">
+        </div>
+        <div class="col-span-12 flex">
+          <img src="/colher de pau.png" alt="Pote" class="w-6/12">
+          <img src="/colher ovo.png" alt="Pote" class="w-6/12">
+        </div>
+      </div>
+      <Card class="col-span-6 h-[calc(100vh-2rem)] bg-[#cdc4b3] shadow-[10px_10px_20px_rgba(0,0,0,0.8)] my-4">
+        <CardHeader>
+          <div class="grid grid-cols-3">
+            <div>
+              <div class="grid grid-cols-2">
+                <img src="/folha.png" alt="Pote" class="rotate-12">
+                <img src="/pote.png" alt="Pote" class="rotate-12">
+                <img src="/pote.png" alt="Pote" class="rotate-12">
+                <img src="/pote.png" alt="Pote" class="rotate-12">
+              </div>
+              <div class="justify-center">
+                <img src="/pote.png" alt="Pote" class="rotate-12">
+              </div>
+              <div class="grid grid-cols-4">
+                <img src="/suculenta.png" alt="Folha">
+                <img src="/colher de pau.png" alt="Pote" class="rotate-180">
+                <img src="/colher ovo.png" alt="ColherOvo">
+              </div>
+            </div>
+            <div>
+              <CardTitle class="text-5xl text-nowrap text-center font-PlayfairDisplay flex justify-center items-center">
+                <img src="/buque.png" alt="Folha" class="w-14">
+                CHÁ DE
+                <img src="/buque.png" alt="Folha" class="w-14">
+              </CardTitle>
+              <CardTitle class="text-5xl text-nowrap text-center font-PlayfairDisplay flex justify-center items-center">CASA NOVA</CardTitle>
+              <div class="flex justify-center">
+                <Separator class="my-4 bg-[#856741] w-6/12"/>
+                🥄
+                <Separator class="my-4 bg-[#856741] w-6/12"/>
+              </div>
+              <CardTitle class="text-3xl text-center font-PlayfairDisplay text-nowrap pb-4">IOHANNA & BRUNO</CardTitle>
+              <Card class="bg-[#e4d8c3]">
+                <CardHeader>
+                  <CardTitle>
+                    Deixe uma mensagem para o casal
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <Caderno class="h-64" v-model="mensagemCasal"/>
+                </CardContent>
+              </Card>
+            </div>
+            <div>
+              <div class="pl-14">
+                <Card class="w-8/12 h-6/6 text-center bg-[#a38967]" style="cursor: pointer;" @click="teste">
+                  <div class="pl-4 pr-4">
+                    <div class="font-PlayfairDisplay mt-4">
+                      LISTA DE PRESENTE
+                    </div>
+                    <Separator class="mb-4 bg-[#856741]"/>
+                    <div class="pb-4">
+                      asdasd
+                    </div>
+                  </div>
+                </Card>
+              </div>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+        </CardContent>
+      </Card>
+      <div class="col-span-3 grid grid-cols-12 items-center">
+        <img src="/folha.png" alt="Pote" class="-rotate-45 col-span-6">
+        <img src="/suculenta.png" alt="Pote" class="rotate-12 col-span-6">
+        <img src="/pote.png" alt="Pote" class="rotate-12 col-span-12">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
+import Caderno from '@/components/Caderno.vue'
+
+const mensagemCasal = ref('')
+
+const teste = () => {
+  console.log('passei')
+}
+const lista = ref([{}])
+</script>
---
+++ src/components/Caderno.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="notebook-page">
+    <textarea v-model="textContent" class="notebook-textarea"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FolhaDeCaderno',
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    textContent: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Estilo geral da página do caderno */
+.notebook-page {
+  position: relative;
+  background-color: #fdfdfd;
+  margin: 1rem auto;
+  padding: 1rem;
+  border-radius: 8px;
+  box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+}
+
+/* Linhas horizontais simulando uma folha de caderno */
+.notebook-page::before {
+  content: "";
+  position: absolute;
+  top: 1rem;
+  left: 0.5rem;
+  width: calc(100% - 0.5rem);
+  height: 100%;
+  background-image: repeating-linear-gradient(
+    to bottom,
+    #e0e0e0 0px,
+    #e0e0e0 1px,
+    transparent 1px,
+    transparent 30px
+  );
+  pointer-events: none;
+  z-index: 1;
+}
+
+/* Bordas perfuradas na lateral esquerda */
+.notebook-page::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0.5rem;
+  height: 100%;
+  background-image: radial-gradient(circle, #ccc 2px, transparent 2px);
+  background-size: 8px 30px;
+  z-index: 1;
+}
+
+/* Estilo do textarea com linhas e bordas */
+.notebook-textarea {
+  position: relative;
+  z-index: 2;
+  background: transparent;
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  resize: none;
+  font-size: 1rem;
+  line-height: 1.9rem;
+  padding-left: 0.5rem;
+  color: #333;
+}
+</style>
