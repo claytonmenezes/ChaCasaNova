18c0274a507f2f9bb2aed564e050b0a50fe04a5c
Clayton Menezes dos Santos <claytonmenezes@hotmail.com>
2024-11-02 20:42:31 -0300

    finalmenteeeeeeee

--- src/App.vue
+++ src/App.vue
@@ -1,127 +1,82 @@
 <template>
-  <div>
-    <div v-if="!ehCelular" class="bg-[#a38967] h-screen">
-      <div class="grid grid-cols-12">
-        <div class="col-span-3">
-          <img src="/juntas.png" alt="Pote" style="object-fit: cover" class="h-screen">
-        </div>
-        <div class="col-span-6">
-          <Card class="h-screen bg-[#cdc4b3] shadow-[10px_10px_20px_rgba(0,0,0,0.8)]">
-            <CardHeader>
-              <div class="grid grid-cols-12">
-                <div class="col-span-4">
-                  <div class="grid grid-cols-2">
-                    <div class="pt-10 2xl:pt-0">
-                      <img src="/colher de pau 2.png" alt="Pote" class="rotate-45 w-14 ml-8">
-                      <img src="/prato.png" alt="Pote" class="rotate-12 w-32 ">
-                    </div>
-                    <div class="pt-10 2xl:pt-0">
-                      <img src="/bule 2.png" alt="Pote" class="rotate-12 lg:pr-6">
-                      <img src="/xicara.png" alt="Pote" class="rotate-12">
-                    </div>
-                  </div>
-                  <div class="justify-center pt-10 2xl:pt-0">
-                    <img src="/taboaCarne.png" alt="Pote" class="rotate-12">
-                  </div>
-                </div>
-                <div class="col-span-4">
-                  <CardTitle class="text-5xl text-nowrap text-center font-PlayfairDisplay flex justify-center items-center">
-                    <img src="/buque.png" alt="Folha" class="w-14">
-                    CHÁ DE
-                    <img src="/buque.png" alt="Folha" class="w-14">
-                  </CardTitle>
-                  <CardTitle class="text-5xl text-nowrap text-center font-PlayfairDisplay flex justify-center items-center">CASA NOVA</CardTitle>
-                  <div class="flex justify-center">
-                    <Separator class="my-4 bg-[#856741] w-6/12"/>
-                    🥄
-                    <Separator class="my-4 bg-[#856741] w-6/12"/>
-                  </div>
-                  <CardTitle class="text-3xl text-center font-PlayfairDisplay text-nowrap pb-4">IOHANNA & BRUNO</CardTitle>
-                  <Card class="bg-[#e4d8c3] w-64 2xl:w-full">
-                    <CardHeader>
-                      <CardTitle class="text-center">
-                        Deixe uma mensagem para o casal
-                      </CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                      <Caderno class="h-64" v-model="mensagemCasal"/>
-                    </CardContent>
-                  </Card>
-                </div>
-                <div class="col-span-4">
-                  <Card class="ml-14 2xl:ml-0 mb-28 2xl:mb-10 font-PlayfairDisplay bg-[#cfb899]" style="cursor: pointer;" @click="teste">
-                    <CardHeader>
-                      <CardTitle class="text-center">
-                        LISTA DE PRESENTE
-                      </CardTitle>
-                    </CardHeader>
-                  </Card>
-                  <Foto class="pl-10 2xl:pl-0 pb-6 2xl:pb-0" :photos="['1.jpg', '2.jpg']"/>
-                  <Foto class="pl-14 pb-6 2xl:pb-0" :photos="['3.jpg', '6.jpg']"/>
-                  <Foto class="pl-10 2xl:pl-0" :photos="['4.jpg', '5.jpg']"/>
-                </div>
+  <div class="grid grid-cols-12 items-center justify-center bg-[#a38967] h-screen" style="background-image: url('/juntas.png')">
+    <div v-if="!ehCelular" class="col-span-3"></div>
+    <Card class="bg-[#cdc4b3] shadow-[10px_10px_20px_rgba(0,0,0,0.8)] col-span-6">
+      <CardHeader>
+        <div class="grid grid-cols-12">
+          <div class="col-span-4">
+            <div class="grid grid-cols-2">
+              <div class="pt-10 2xl:pt-0">
+                <img src="/colher de pau 2.png" alt="Pote" class="rotate-45 w-14 ml-8">
+                <img src="/prato.png" alt="Pote" class="rotate-12 w-32 ">
               </div>
-            </CardHeader>
-            <CardContent class="text-center">
-              asdasdasdasdasdasd
-            </CardContent>
-          </Card>
-        </div>
-        <div class="col-span-3">
-          <img src="/juntas.png" alt="Pote" style="object-fit: cover" class="h-screen">
-        </div>
-      </div>
-    </div>
-    <div v-else class="bg-[#a38967] h-screen">
-      <Card class="bg-[#cdc4b3] h-screen">
-        <CardHeader>
-          <CardTitle class="text-5xl text-nowrap text-center font-PlayfairDisplay flex justify-center items-center">
-            <img src="/buque.png" alt="Folha" class="w-14">
-            CHÁ DE
-            <img src="/buque.png" alt="Folha" class="w-14">
-          </CardTitle>
-          <CardTitle class="text-5xl text-nowrap text-center font-PlayfairDisplay flex justify-center items-center">CASA NOVA</CardTitle>
-          <div class="flex justify-center">
-            <Separator class="my-4 bg-[#856741] w-6/12"/>
-            🥄
-            <Separator class="my-4 bg-[#856741] w-6/12"/>
-          </div>
-          <CardTitle class="text-3xl text-center font-PlayfairDisplay text-nowrap pb-4">IOHANNA & BRUNO</CardTitle>
-        </CardHeader>
-        <Card class="bg-[#e4d8c3]">
-          <CardHeader>
-            <CardTitle class="text-center">
-              Deixe uma mensagem para o casal
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Caderno class="h-64" v-model="mensagemCasal"/>
-          </CardContent>
-        </Card>
-        <Card class="my-4 text-center bg-[#cfb899]" style="cursor: pointer;" @click="teste">
-          <div class="pl-4 pr-4">
-            <div class="font-PlayfairDisplay m-4">
-              LISTA DE PRESENTE
+              <div class="pt-10 2xl:pt-0">
+                <img src="/bule 2.png" alt="Pote" class="rotate-12 lg:pr-6">
+                <img src="/xicara.png" alt="Pote" class="rotate-12">
+              </div>
+            </div>
+            <div class="justify-center pt-10 2xl:pt-0">
+              <img src="/taboaCarne.png" alt="Pote" class="rotate-12">
             </div>
           </div>
-        </Card>
-      </Card>
-    </div>
-    <Dialog v-model:open="dialogListaPresente">
-      <DialogContent class="bg-[#cdc4b3] font-PlayfairDisplay h-screen max-w-4xl">
-        <DialogHeader>
-          <DialogTitle class="text-6xl text-center">Lista de Presente</DialogTitle>
-          <DialogDescription/>
-          <div class="flex justify-center">
-            <Separator class="my-4 bg-[#856741] w-6/12"/>
-            🥄
-            <Separator class="my-4 bg-[#856741] w-6/12"/>
+          <div class="col-span-4">
+            <CardTitle class="text-5xl text-nowrap text-center font-PlayfairDisplay flex justify-center items-center">
+              <img src="/buque.png" alt="Folha" class="w-14">
+              CHÁ DE
+              <img src="/buque.png" alt="Folha" class="w-14">
+            </CardTitle>
+            <CardTitle class="text-5xl text-nowrap text-center font-PlayfairDisplay flex justify-center items-center">CASA NOVA</CardTitle>
+            <div class="flex justify-center">
+              <Separator class="my-4 bg-[#856741] w-6/12"/>
+              🥄
+              <Separator class="my-4 bg-[#856741] w-6/12"/>
+            </div>
+            <CardTitle class="text-3xl text-center font-PlayfairDisplay text-nowrap pb-4">IOHANNA & BRUNO</CardTitle>
+            <Card class="bg-[#e4d8c3] w-64 2xl:w-full">
+              <CardHeader>
+                <CardTitle class="text-center">
+                  Deixe uma mensagem para o casal
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <Input placeholder="Seu nome" v-model="nomePessoa" class="bg-white"/>
+                <Caderno class="h-64" v-model="mensagemCasal"/>
+              </CardContent>
+            </Card>
           </div>
-        </DialogHeader>
-        <PresentList/>
-      </DialogContent>
-    </Dialog>
+          <div class="col-span-4">
+            <Card class="ml-14 2xl:ml-0 mb-28 2xl:mb-10 font-PlayfairDisplay bg-[#cfb899]" style="cursor: pointer;" @click="abrirListaPresente">
+              <CardHeader>
+                <CardTitle class="text-center">
+                  LISTA DE PRESENTE
+                </CardTitle>
+              </CardHeader>
+            </Card>
+            <Foto class="pl-10 2xl:pl-0 pb-6 2xl:pb-0" :photos="['1.jpg', '2.jpg']"/>
+            <Foto class="pl-14 pb-6 2xl:pb-0" :photos="['3.jpg', '6.jpg']"/>
+            <Foto class="pl-10 2xl:pl-0" :photos="['4.jpg', '5.jpg']"/>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent class="text-center">
+        asdasdasdasdasdasdasdasd
+      </CardContent>
+    </Card>
   </div>
+  <Dialog v-model:open="dialogListaPresente">
+    <DialogContent class="bg-[#cdc4b3] font-PlayfairDisplay h-screen max-w-4xl">
+      <DialogHeader>
+        <DialogTitle class="text-6xl text-center">Lista de Presente</DialogTitle>
+        <DialogDescription/>
+        <div class="flex justify-center">
+          <Separator class="my-4 bg-[#856741] w-6/12"/>
+          🥄
+          <Separator class="my-4 bg-[#856741] w-6/12"/>
+        </div>
+      </DialogHeader>
+      <PresentList :grupos="lista"/>
+    </DialogContent>
+  </Dialog>
 </template>
 
 <script setup>
@@ -129,12 +84,15 @@ import { ref, onMounted } from 'vue'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 import Caderno from '@/components/Caderno.vue'
 import Foto from '@/components/Foto.vue'
 import { createClient } from '@supabase/supabase-js'
 import PresentList from '@/components/PresentList.vue'
 
 const lista = ref([])
+const nomePessoa = ref('')
 const ehCelular = ref(false)
 const dialogListaPresente = ref(false)
 const ehMobile = () => {
@@ -144,15 +102,30 @@ const ehMobile = () => {
 
 const mensagemCasal = ref('')
 
-const teste = () => {
+const abrirListaPresente = () => {
   dialogListaPresente.value = true
 }
 const supabase = createClient('https://acfrcmkzzabbqtwchiow.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjZnJjbWt6emFiYnF0d2NoaW93Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMDQ3NzE3OCwiZXhwIjoyMDQ2MDUzMTc4fQ.ZoWVFkjTxFJL5ox3Zb-R4hgk7t6ia9T639aJEFZF4js')
 
+const agruparPorGrupo = (presentes) => {
+  return presentes.reduce((acc, presente) => {
+    const { grupo, nome, qtde } = presente
+
+    // Se o grupo ainda não existe no acumulador, cria um novo
+    if (!acc[grupo]) {
+      acc[grupo] = { nome: grupo, presentes: [] }
+    }
+
+    // Adiciona o nome do item ao array de itens do grupo
+    acc[grupo].presentes.push({nome, qtde})
+
+    return acc
+  }, {})
+}
+
 onMounted(async () => {
   ehMobile()
   const { data, error } = await supabase.from('presente').select('*')
-  lista.value = data
-  console.log(lista.value)
+  lista.value = agruparPorGrupo(data)
 })
 </script>
--- src/components/PresentList.vue
+++ src/components/PresentList.vue
@@ -1,12 +1,12 @@
 <template>
   <ScrollArea class="h-full">
-    <div class="grid grid-cols-2 md:grid-cols-3 gap-8">
-      <div v-for="category in categories" :key="category.title" class="bg-white p-6 rounded-lg shadow">
-        <h2 class="text-lg font-semibold mb-4 text-center border-b border-gray-300 pb-2">{{ category.title }}</h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div v-for="(grupo, index) in grupos" :key="index" class="bg-white p-6 rounded-lg shadow">
+        <h2 class="text-lg font-semibold mb-4 text-center border-b border-gray-300 pb-2">{{ grupo.nome }}</h2>
         <ul class="space-y-2">
-          <li v-for="item in category.items" :key="item" class="flex items-center">
+          <li v-for="(presente, indexPresente) in grupo.presentes" :key="indexPresente" class="flex items-center">
             <span class="mr-2">•</span>
-            <span>{{ item }}</span>
+            <span>{{ presente.nome }}</span>
           </li>
         </ul>
       </div>
@@ -16,55 +16,14 @@
 
 <script setup>
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { defineProps } from 'vue'
 
-const categories = [
-  {
-    title: "Cozinha",
-    items: [
-      "Abridor de lata", "Açucareiro", "Formas e assadeiras", "Afiador de facas",
-      "Panela de pressão", "Descanso de panelas", "Conjunto de xícaras", "Porta condimentos",
-      "Espremedor", "Leiteira", "Copo de medida", "Ralador", "Saca rolhas",
-      "Pegador de macarrão", "Porta papel toalha", "Porta guardanapo", "Manteigueira",
-      "Panos de prato", "Avental", "Lixeiras de cozinha", "Chaleira", "Colher de pau",
-      "Colher de sorvete", "Concha", "Jogo de panelas, pratos e talheres", "Cortador de queijo",
-      "Escorredor de louça/macarrão/talheres", "Escumadeira", "Faca de pão", "Frigideira",
-      "Tábua para cortes", "Potes multiuso", "Jarra", "Garrafa térmica", "Luva térmica",
-      "Porta detergente e esponja", "Pano de chão tipo saco", "Conjunto para sobremesa",
-      "Toalha de mesa", "Rodinho de pia", "Ralador", "Saleiro", "Jogo americano"
-    ]
-  },
-  {
-    title: "Sala",
-    items: [
-      "Cortina para sala", "Mesa de centro", "Prateleiras", "Luminária",
-      "Mantas e almofadas", "Tapete", "Quadros decorativos", "Suporte para televisão",
-      "Capa para sofá"
-    ]
-  },
-  {
-    title: "Banheiro",
-    items: [
-      "Porta escova de dentes", "Tapete antiderrapante para box", "Cortina para banheiro",
-      "Toalha para mãos", "Toalhas de banho", "Toalhas de rosto", "Capacho",
-      "Cesto para roupa", "Escova para vaso sanitário", "Lixeira", "Saboneteira", "Roupão"
-    ]
-  },
-  {
-    title: "Área de Serviço",
-    items: [
-      "Baldes de plástico", "Pá de lixo", "Pano de chão", "Porta sabão",
-      "Pregadores de roupa", "Rodo", "Vassouras", "Esfregão", "Escovinha de limpeza"
-    ]
-  },
-  {
-    title: "Itens Decorativos",
-    items: [
-      "Espelhos", "Almofadas", "Luminárias", "Relógio de parede", "Porta chaves",
-      "Aparador de livros", "Porta retrato", "Vela aromatizadora", "Difusor", "Vasos decorativos"
-    ]
+defineProps({
+  grupos: {
+    type: Object,
+    required: true,
+    default: () => []
   }
-]
-</script>
+})
 
-<style scoped>
-</style>
+</script>
